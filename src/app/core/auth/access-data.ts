ac43e47e0f4a4f25a5bb8906562d3bf5e423f536
D0109 <sq.luo@siid.ac.cn>
2018-09-14 09:53:53 +0800

    种子

---
+++ src/app/core/auth/access-data.ts
@@ -0,0 +1,7 @@
+// Samplce access data
+// {"id":1,"username":"admin","password":"demo","email":"admin@demo.com","accessToken":"access-token-0.022563452858263444","refreshToken":"access-token-0.9348573301432961","roles":["ADMIN"],"pic":"./assets/app/media/img/users/user4.jpg","fullname":"Mark Andre"}
+export interface AccessData {
+	accessToken: string;
+	refreshToken: string;
+	roles: any;
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  isMockEnabled: true // You have to switch this, when your real back-end is done
+};
